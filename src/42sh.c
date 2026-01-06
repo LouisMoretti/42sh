7089bedf9f2c755b669d6e6c1c80b3671f3b4190
arnaud.bellicha <arnaud.bellicha@epita.fr>
2026-01-06 23:13:51 +0100

    add: iobackend

--- src/42sh.c
+++ src/42sh.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
-
 #include "config/config.h"
+#include "iobackend/iobackend.h"
 
 int main(int argc, char **argv)
 {
     if (set_conf(argc, argv) != 0)
         return 2;
-    // call io_setup
+    io_setup();
 
+    io_close();
     return 0;
 }
