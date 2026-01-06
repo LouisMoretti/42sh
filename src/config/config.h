23bb726f58cb6f306a93ddd634b42641a462fda6
arnaud.bellicha <arnaud.bellicha@epita.fr>
2026-01-06 22:26:45 +0100

    add: config module and main

---
+++ src/config/config.h
@@ -0,0 +1,20 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+enum input_method
+{
+    STRING,
+    STDIN,
+    FILEPATH
+};
+
+struct config
+{
+    enum input_method method;
+    char *str_stream;
+};
+
+int set_conf(int argc, char **argv);
+struct config *get_conf(void);
+
+#endif // ! CONFIG_H
