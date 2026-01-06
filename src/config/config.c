e2a01b874e3e308c6deea65667cdbc61c492e2b0
arnaud.bellicha <arnaud.bellicha@epita.fr>
2026-01-06 22:26:45 +0100

    add: config module and main

---
+++ src/config/config.c
@@ -0,0 +1,34 @@
+#include "config.h"
+static struct config my_conf;
+
+int set_conf(int argc, char **argv)
+{
+    my_conf.method = STDIN; // Default method is stdin
+    int i = 1;
+    while (i < argc)
+    {
+        if (argv[i][0] == '-')
+        {
+            if (argv[i][1] == 'c')
+            {
+                if (i + 1 >= argc)
+                    return 2;
+                my_conf.method = STRING;
+                my_conf.str_stream = argv[i + 1];
+            }
+            // Verbose - Pretty Print
+        }
+        else
+        {
+            my_conf.method = FILEPATH;
+            my_conf.str_stream = argv[i];
+        }
+        i++;
+    }
+    return 0;
+}
+
+struct config *get_conf(void)
+{
+    return &my_conf;
+}
