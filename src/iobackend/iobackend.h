82468fa47142b3833afd300341b034f09fcc2b1a
arnaud.bellicha <arnaud.bellicha@epita.fr>
2026-01-06 23:13:51 +0100

    add: iobackend

---
+++ src/iobackend/iobackend.h
@@ -0,0 +1,11 @@
+#ifndef IOBACKGROUND_H
+#define IOBACKGROUND_H
+
+int peek_chr(void);
+void pop_chr(void);
+int get_chr(void);
+int io_setup(void);
+void io_close(void);
+
+#endif // ! IOBACKGROUND_H
+
