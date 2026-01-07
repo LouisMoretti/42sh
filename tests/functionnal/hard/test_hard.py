afe6af0d28586a8ed14dc076af6179871b17a21d
Bisiaux <clement.bisiaux@epita.fr>
2026-01-07 17:56:40 +0100

    UPDATE: hard test added

--- tests/functionnal/hard/test_hard.py
+++ tests/functionnal/hard/test_hard.py
@@ -40,3 +40,25 @@ def test_many_if():
     finally:
         kill_42sh(proc)
         kill_42sh(ref_proc)
+
+
+
+# ----------------------
+#         MIXED
+# ----------------------
+
+def test_mixed_demon():
+    command_to_run = "if true; then echo -n G; fi; if true; then echo -n G; echo -n ' '; fi; if false; then echo Aie; else echo !; fi; if echo -n You; then echo -n ' '; else echo booooo; fi; if true; then if false; then echo noobs; else echo -n are; fi fi; if false; then false; elif echo -n ' soooo '; then echo strong !; else true; fi; echo -E that is an \# btw, not the next one here : # stupid human;"
+    proc = run_command_string(command_to_run)
+    ref_proc = run_ref_command(command_to_run)
+    try:
+        out, err = proc.communicate(timeout=1)
+        ref_out, ref_err = ref_proc.communicate(timeout=1)
+        assert out == ref_out
+        assert err == ref_err
+        assert proc.returncode == ref_proc.returncode
+    finally:
+        kill_42sh(proc)
+        kill_42sh(ref_proc)
+
+
