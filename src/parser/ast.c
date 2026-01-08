3f4c3ba283156f38d63179476241fc4a18b90057
Louis Moretti <louis.moretti@epita.fr>
2026-01-08 23:33:06 +0100

    add: parser.h: simple_cmd

--- src/parser/ast.c
+++ src/parser/ast.c
@@ -1,1 +1,28 @@
-void to_delete(void);
+#include "ast.h"
+
+#include <stdlib.h>
+
+struct ast_simple_cmd *init_ast_simple_cmd()
+{
+    struct ast_simple_cmd *cmd = malloc(sizeof(struct ast_simple_cmd));
+
+    cmd->base.type = AST_SIMPLE_CMD;
+    cmd->element_list = NULL;
+    cmd->prefix = NULL;
+    cmd->prefix_list = NULL;
+    cmd->word = NULL;
+
+    return cmd;
+}
+
+struct element_list *init_element_list()
+{
+    struct element_list *list = malloc(sizeof(struct element_list));
+
+    list->element.base.type = AST_ELEMENT;
+    list->element.word = NULL;
+    list->element.redirection = NULL;
+    list->next = NULL;
+
+    return list;
+}
