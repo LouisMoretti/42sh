ea8dfbc802cf7ad35fe58c1bd7af5d2f62f44a3a
Yassine <yassine.tlich@epita.fr>
2026-01-09 22:41:43 +0100

    Add: True & False builtins

--- src/execution/boolean.c
+++ src/execution/boolean.c
@@ -1,1 +1,11 @@
 #include "builtin.h"
+
+int builtin_false(struct ast_simple_cmd *ast_simple_cmd)
+{
+    return 1;
+}
+
+int builtin_true(struct ast_simple_cmd *ast_simple_cmd)
+{
+    return 0;
+}
