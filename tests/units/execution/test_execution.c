c7a148f97058874689d72845e989890bfd8e6e6d
Bisiaux <clement.bisiaux@epita.fr>
2026-01-09 23:54:29 +0100

    ADD: testsuite execution

--- tests/units/execution/test_execution.c
+++ tests/units/execution/test_execution.c
@@ -2,13 +2,40 @@
 #include <criterion/new/assert.h>
 #include <criterion/parameterized.h>
 #include <criterion/redirect.h>
+#include <stddef.h>
 #include <string.h>
 
 #include "config/config.h"
-#include "iobackend/iobackend.h"
-#include "lexer/lexer.h"
+#include "parser/ast.h"
 
 TestSuite(Execution);
 
 Test(Execution, simple_cmd)
-{}
+{
+    struct ast_simple_cmd *ast_simple_cmd =
+        malloc(sizeof(struct ast_simple_cmd));
+
+    ast_simple_cmd->prefix_list = NULL;
+    ast_simple_cmd->prefix = NULL;
+    ast_simple_cmd->word = "ls";
+
+    struct ast_element_list *ast_element_list =
+        malloc(sizeof(struct ast_element_list));
+
+    ast_element_list->base = AST_ELEMENT_LIST;
+
+    struct ast_element *ast_element = malloc(sizeof(struct ast_element));
+    ast_element->base = AST_ELEMENT;
+    ast_element->word = "-a";
+    ast_element->redirection = NULL;
+
+    ast_element_list->element = ast_element;
+    ast_element_list->next = NULL;
+
+    ast_simple_cmd->element_list = ast_element_list;
+
+    struct ast *ast = (struct ast *)ast_simple_cmd;
+
+    int res = execute_ast(ast);
+    cr_expect(res == 0);
+}
