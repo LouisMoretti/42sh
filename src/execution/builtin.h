d090a9a22005273c123b2ffc34062e66773ac200
Yassine <yassine.tlich@epita.fr>
2026-01-10 00:01:44 +0100

    Fix: Execution Include in Build

--- src/execution/builtin.h
+++ src/execution/builtin.h
@@ -1,6 +1,8 @@
 #ifndef BUILTIN_H
 #define BUILTIN_H
 
+#include "parser/ast.h"
+
 int builtin_echo(struct ast_simple_cmd *command);
 int builtin_false(struct ast_simple_cmd *ast_simple_cmd);
 int builtin_true(struct ast_simple_cmd *ast_simple_cmd);
