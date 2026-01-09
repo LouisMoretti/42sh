e51e3486ff9e92c4909244eed2a41a909c47bdcc
Louis Moretti <louis.moretti@epita.fr>
2026-01-09 06:22:52 +0100

    update: parser now parse acording to grammar (limited to simple command for now)

--- src/parser/parser.c
+++ src/parser/parser.c
@@ -1,4 +1,6 @@
 #define _POSIX_C_SOURCE 200809L
+#include "parser/parser.h"
+
 #include <assert.h>
 #include <stddef.h>
 #include <string.h>
@@ -6,7 +8,29 @@
 #include "lexer/lexer.h"
 #include "parser/ast.h"
 
-struct ast_simple_cmd *simple_cmd()
+struct ast_input *parse_input()
+{
+    struct ast_input *ast_input = init_ast_input();
+
+    struct ast_list *list = init_ast_list();
+    ast_input->list = (struct ast *)list;
+
+    struct ast_and_or *and_or = init_ast_and_or();
+    list->and_or = (struct ast *)and_or;
+
+    struct ast_pipeline *pipeline = init_ast_pipeline();
+    and_or->pipeline = (struct ast *)pipeline;
+
+    struct ast_cmd *cmd = init_ast_cmd();
+    pipeline->cmd = (struct ast *)cmd;
+
+    struct ast_simple_cmd *simple_command = parse_simple_cmd();
+    cmd->cmd = (struct ast *)simple_command;
+
+    return ast_input;
+}
+
+struct ast_simple_cmd *parse_simple_cmd()
 {
     struct ast_simple_cmd *cmd = init_ast_simple_cmd();
 
