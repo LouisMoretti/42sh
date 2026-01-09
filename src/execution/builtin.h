1aa6de11933419812957502dff524be666c6b8c9
Yassine <yassine.tlich@epita.fr>
2026-01-09 22:41:22 +0100

    Add: builtin functions header

---
+++ src/execution/builtin.h
@@ -0,0 +1,8 @@
+#ifndef BUILTIN_H
+#define BUILTIN_H
+
+int builtin_echo(struct ast_simple_cmd *command);
+int builtin_false(struct ast_simple_cmd *ast_simple_cmd);
+int builtin_true(struct ast_simple_cmd *ast_simple_cmd);
+
+#endif // ! BUILTIN_H
