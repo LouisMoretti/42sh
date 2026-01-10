47add8fd996f3b26e32913c9436e1b22cb8c2ace
Yassine <yassine.tlich@epita.fr>
2026-01-10 17:05:00 +0100

    Update: Add of shell_cmd, if-else_if-else and compound list execution

--- src/execution/execution.c
+++ src/execution/execution.c
@@ -15,6 +15,8 @@
 
 typedef int (*exec)(struct ast *);
 
+static int execute_ast_shell_cmd(struct ast *ast);
+
 static int evaluate_command(char **command)
 {
     int pid = fork();
@@ -96,8 +98,15 @@ static int execute_ast_cmd(struct ast *ast)
     assert(ast->type == AST_CMD);
     struct ast_cmd *ast_cmd = (struct ast_cmd *)ast;
     assert(ast_cmd->cmd != NULL);
-
-    return execute_ast_simple_cmd(ast_cmd->cmd);
+    switch (ast_cmd->cmd->type)
+    {
+    case AST_SIMPLE_CMD:
+        return execute_ast_simple_cmd(ast_cmd->cmd);
+    case AST_SHELL_CMD:
+        return execute_ast_shell_cmd(ast_cmd->cmd);
+    default: // May not fall through
+        return 1;
+    }
 }
 
 static int execute_ast_pipeline(struct ast *ast)
@@ -122,6 +131,76 @@ static int execute_ast_and_or(struct ast *ast)
     assert(ast_and_or->pipeline != NULL);
 
     return execute_ast_pipeline(ast_and_or->pipeline);
+}
+
+static int execute_ast_compound_list(struct ast *ast)
+{
+    if (!ast)
+        return 0;
+
+    assert(ast->type == AST_COMPOUND_LIST);
+    struct ast_compound_list *ast_compound_list =
+        (struct ast_compound_list *)ast;
+    assert(ast_compound_list->ast_and_or != NULL);
+    if (!ast_compound_list->next)
+    {
+        return execute_ast_and_or(ast_compound_list->ast_and_or);
+    }
+    else
+    {
+        execute_ast_and_or(ast_compound_list->ast_and_or);
+
+        return execute_ast_compound_list(ast_compound_list->next);
+    }
+}
+
+static int execute_ast_else_clause(struct ast *ast)
+{
+    if (!ast)
+        return 0;
+
+    assert(ast->type == AST_CLAUSE_ELSE);
+    struct ast_else_clause *ast_else_clause = (struct ast_else_clause *)ast;
+    assert(ast_else_clause->body_compound_list != NULL);
+    if (!ast_else_clause->else_clause)
+        assert(ast_else_clause->condition_compound_list != NULL);
+
+    int condition_exit_code =
+        execute_ast_compound_list(ast_else_clause->condition_compound_list);
+    if (!condition_exit_code)
+        return execute_ast_compound_list(ast_else_clause->body_compound_list);
+    else
+        return execute_ast_else_clause(ast_else_clause->else_clause);
+}
+
+static int execute_ast_rule_if(struct ast *ast)
+{
+    if (!ast)
+        return 0;
+
+    assert(ast->type == AST_RULE_IF);
+    struct ast_rule_if *ast_rule_if = (struct ast_rule_if *)ast;
+    assert(ast_rule_if->condition_compound_list != NULL);
+    assert(ast_rule_if->body_compound_list != NULL);
+
+    int condition_exit_code =
+        execute_ast_compound_list(ast_rule_if->condition_compound_list);
+    if (!condition_exit_code)
+        return execute_ast_compound_list(ast_rule_if->body_compound_list);
+    else
+        return execute_ast_else_clause(ast_rule_if->else_clause);
+}
+
+static int execute_ast_shell_cmd(struct ast *ast)
+{
+    if (!ast)
+        return 0;
+
+    assert(ast->type == AST_SHELL_CMD);
+    struct ast_shell_cmd *ast_shell_cmd = (struct ast_shell_cmd *)ast;
+    assert(ast_shell_cmd->rule != NULL);
+
+    return execute_ast_rule_if(ast_shell_cmd->rule);
 }
 
 static int execute_ast_list(struct ast *ast)
@@ -154,14 +233,18 @@ static int execute_ast_input(struct ast *ast)
     return execute_ast_list(ast_input->list);
 }
 
-static exec execute_functions[] = {
-    [AST_INPUT] = &execute_ast_input,
-    [AST_LIST] = &execute_ast_list,
-    [AST_AND_OR] = &execute_ast_and_or,
-    [AST_PIPELINE] = &execute_ast_pipeline,
-    [AST_CMD] = &execute_ast_cmd,
-    [AST_SIMPLE_CMD] = &execute_ast_simple_cmd
-};
+static exec execute_functions[] = { [AST_INPUT] = &execute_ast_input,
+                                    [AST_LIST] = &execute_ast_list,
+                                    [AST_AND_OR] = &execute_ast_and_or,
+                                    [AST_PIPELINE] = &execute_ast_pipeline,
+                                    [AST_CMD] = &execute_ast_cmd,
+                                    [AST_SIMPLE_CMD] = &execute_ast_simple_cmd,
+                                    [AST_SHELL_CMD] = &execute_ast_shell_cmd,
+                                    [AST_COMPOUND_LIST] =
+                                        &execute_ast_compound_list,
+                                    [AST_RULE_IF] = &execute_ast_rule_if,
+                                    [AST_CLAUSE_ELSE] =
+                                        &execute_ast_else_clause };
 
 int execute_ast(struct ast *ast)
 {
