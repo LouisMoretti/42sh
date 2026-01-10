784bf2e2122593001d8326f852d4a4c64af52d94
Arnaud Bellicha <arnaud.bellicha@epita.fr>
2026-01-10 16:24:31 +0100

    add:draft-echo

--- src/execution/echo.c
+++ src/execution/echo.c
@@ -38,16 +38,25 @@ static int update_flags(char *str, int *has_n, int *has_e, int *has_E)
 
 int builtin_echo(struct ast_simple_cmd *command)
 {
-    struct ast_element_list *cur =
-        (struct ast_element_list *)command->element_list;
     int has_n = 0;
     int has_e = 0;
     int has_E = 0;
+    struct ast_element_list *cur =
+        (struct ast_element_list *)command->element_list;
     int has_left_flags = 0;
+    struct ast_element *ast_element = (struct ast_element *)cur->element;
+    char *str = ast_element->word;
+    if (str[0] != '-' || !update_flags(str, &has_n, &has_e, &has_E))
+    {
+        has_left_flags = 1;
+        printf("%s");
+        fflush(stdout);
+    }
+    cur = (struct ast_element_list *)cur->next;
     while (cur != NULL)
     {
-        struct ast_element *ast_element = (struct ast_element *)cur->element;
-        char *str = ast_element->word;
+        ast_element = (struct ast_element *)cur->element;
+        str = ast_element->word;
         if (!has_left_flags)
         {
             if (str[0] != '-' || !update_flags(str, &has_n, &has_e, &has_E))
@@ -55,14 +64,12 @@ int builtin_echo(struct ast_simple_cmd *command)
         }
         if (has_left_flags)
         {
-            if (has_n)
-                printf("%s", str);
-            else
-                printf("%s\n", str);
+            printf(" %s", str);
+            fflush(stdout);
         }
-
-        fflush(stdout);
         cur = (struct ast_element_list *)cur->next;
     }
+    if(!has_n)
+        printf("\n");
     return 0;
 }
