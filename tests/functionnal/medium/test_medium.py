66c8fb0612b6455f641e250fa547f6c598ff73b3
Yassine <yassine.tlich@epita.fr>
2026-01-19 02:24:34 +0100

    Update: Add of several tests for until, while, for loops, negation and or operator Execution

--- tests/functionnal/medium/test_medium.py
+++ tests/functionnal/medium/test_medium.py
@@ -202,19 +202,70 @@ def test_simple_several_commands_and_ands_pipes():
     finally:
         kill_42sh(proc)
         kill_42sh(ref_proc)
+
+def test_while_true():
+    command_to_run = "while true; do echo b; echo Hey hey; done"
+    proc = run_command_string(command_to_run)
+    ref_proc = run_ref_command_string(command_to_run)
+    try:
+        _, _ = proc.communicate(timeout=0.1)
+        _, _ = ref_proc.communicate(timeout=0.1)
+        assert False
+    except sp.TimeoutExpired:
+        assert True
+    finally:
+        kill_42sh(proc)
+        kill_42sh(ref_proc)
+
+def test_until_false():
+    command_to_run = "until false; do echo b; done"
+    proc = run_command_string(command_to_run)
+    ref_proc = run_ref_command_string(command_to_run)
+    try:
+        _, _ = proc.communicate(timeout=0.1)
+        _, _ = ref_proc.communicate(timeout=0.1)
+        assert False
+    except sp.TimeoutExpired:
+        assert True
+    finally:
+        kill_42sh(proc)
+        kill_42sh(ref_proc)
+
+def test_until_true_negated():
+    command_to_run = "until ! true; do echo b; done"
+    proc = run_command_string(command_to_run)
+    ref_proc = run_ref_command_string(command_to_run)
+    try:
+        _, _ = proc.communicate(timeout=0.1)
+        _, _ = ref_proc.communicate(timeout=0.1)
+        assert False
+    except sp.TimeoutExpired:
+        assert True
+    finally:
+        kill_42sh(proc)
+        kill_42sh(ref_proc)
     
 folder = "medium/tests_files"
 param_filepaths = [(open(f"{folder}/{file}", 'r').name, f"{folder}/{file}") for file in os.listdir(folder)]
 params_cmds = [("test_mix_medium","if if if false; then echo Fail\nelif false; then echo Aie\nelif true\nthen if echo Hey\nthen echo -n my; else false\nfi\nfi then ls -a; fi\nthen echo little' poney'; fi # So cooked broo", []),
-        ("simple_redirection", "echo vivetiger > file.txt", []), 
-        ("expansion_before_exec", "var=lol echo $var", []),
-        ("expansion_in_cmd", "txt=ho; ec$txt good", []),
-        ("multiple_expansion", "a=1; b=2; c=3; d=4; echo $a $b $c $d", []),
-        ("argument_var_expansion", "echo $0 $1", ['variable0','variable1']),
-        ("argument_expansion_twodigits", "echo $11", ['variable0','variable1']),
-        ("expansion_bracket","VAR=cletest; echo ${VAR};", []),
-        ("expansion_not_found", "echo $skibidi", []),
-        ("variable_env_to_cmd","PWD=src env | grep PWD", [])]
+               ("simple_redirection", "echo vivetiger > file.txt", []),
+               ("expansion_before_exec", "var=lol echo $var", []),
+               ("expansion_in_cmd", "txt=ho; ec$txt good", []),
+               ("multiple_expansion", "a=1; b=2; c=3; d=4; echo $a $b $c $d", []),
+               ("argument_var_expansion", "echo $0 $1", ['variable0','variable1']),
+               ("argument_expansion_twodigits", "echo $11", ['variable0','variable1']),
+               ("expansion_bracket","VAR=cletest; echo ${VAR};", []),
+               ("expansion_not_found", "echo $skibidi", []),
+               ("simple_while_false", "while false; do echo a; done", []),
+               ('simple_until_true', "until true; do echo 42shhhhh; done", []),
+               ('simple_for_empty', "for var; do echo -n You are cooooooooked !!!!; done", []),
+               ('simple_for_no_expansion', "for v in Hey I am a little poney; do echo -n I am a light sabeeeerrr !; done", []),
+               ('simple_for_with_expansion', "for var in My boat sink ...; do echo $var; done", []),
+               ('many_or_operators', "false || ec || ! echo Aie || echo -n Finally Works !!!", []),
+               ('simple_pipe_with_negation', "! echo a | cat", []),
+               ('many_boolean_operators', "! echo a || echo b && echo c && ! true || echo gg", []),
+               ('pipes_and_or_negation_mixed', "! echo a | cat | cat | false || echo b | cat | cat && echo c && true | false || echo gg | cat | cat | cat", [])
+               ]
 
 
 
