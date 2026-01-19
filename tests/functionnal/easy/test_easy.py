fc79212d8ffdfb1d3651e281792b4e36fab6a994
arnaud.bellicha <arnaud.bellicha@epita.fr>
2026-01-19 21:39:54 +0100

    added: trace_step1

--- tests/functionnal/easy/test_easy.py
+++ tests/functionnal/easy/test_easy.py
@@ -284,7 +284,9 @@ param_filepaths = [(open(f"{folder}/{file}", 'r').name, f"{folder}/{file}") for 
 params_cmds = [("test_escape","echo -e '\\n'"),
                ('simple_negation_false', "! false"),
                ('simple_negation_true', "! true"),
-               ('simple_negation_echo', "! echo a b c")
+               ('simple_negation_echo', "! echo a b c"),
+               ('ending_double_semicol', "echo doublecolonne;;"),
+               ('ending_simple_semicol', "echo simplecolonne;")
                ]
 
 @pytest.mark.parametrize("name,command_to_run", params_cmds)
