28a8391da71ae0ab26c010ff596ac81459364f43
Bisiaux <clement.bisiaux@epita.fr>
2026-01-24 17:09:31 +0100

    fix: hash_map problem malloc and free

--- src/utils/hash_map/hash_map.c
+++ src/utils/hash_map/hash_map.c
@@ -55,7 +55,7 @@ bool hash_map_insert(struct hash_map *hash_map, char *key, char *value,
         *updated = false;
 
     v->next = hash_map->data[place];
-    v->key = key;
+    v->key = strdup(key);
     v->value = strdup(value);
 
     hash_map->data[place] = v;
@@ -68,6 +68,7 @@ static void _free_ll(struct pair_list *ll)
     if (ll == NULL)
         return;
 
+    free(ll->key);
     free(ll->value);
 
     _free_ll(ll->next);
